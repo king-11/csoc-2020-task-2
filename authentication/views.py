60cb54ddcbcadbafaa130c77273910c7546eb348
king-11 <lakshay.singh1108@gmail.com>
2020-05-02 14:27:00 +0530

    login and register added

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
-from django.contrib.auth import login,logout,authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from store.views import index
+from .forms import SignUpForm
 # Create your views here.
 
 
-def loginView(request):
-    pass
-
-def logoutView(request):
-    pass
-
-def registerView(request):
-    pass
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.profile.email = form.cleaned_data.get('email')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
