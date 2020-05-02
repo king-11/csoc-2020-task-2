91c3c73e35d64c48cb7bee5cd1824053913c34a9
king-11 <lakshay.singh1108@gmail.com>
2020-05-02 14:26:35 +0530

    login and register works

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -8,7 +10,8 @@ class Book(models.Model):
     genre = models.CharField(max_length=50)
     description = models.TextField(null=True)
     mrp = models.PositiveIntegerField()
-    rating = models.FloatField(default=0.0)
+    rating = models.IntegerField(
+        default=1, validators=[MaxValueValidator(10), MinValueValidator(0)])
 
     class Meta:
         ordering = ('title',)
@@ -22,11 +25,11 @@ class BookCopy(models.Model):
     borrow_date = models.DateField(null=True, blank=True)
     # True status means that the copy is available for issue, False means unavailable
     status = models.BooleanField(default=False)
-    borrower = models.ForeignKey(User, related_name='borrower', null=True, blank=True, on_delete=models.SET_NULL)
+    borrower = models.ForeignKey(
+        User, related_name='borrower', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         if self.borrow_date:
             return f'{self.book.title}, {str(self.borrow_date)}'
         else:
             return f'{self.book.title} - Available'
-
