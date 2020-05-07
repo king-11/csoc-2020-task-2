dc4e7580aa336a5414f52d2d45eacf7d79aa18d0
king-11 <lakshay.singh1108@gmail.com>
2020-05-07 14:53:17 +0530

    url link change

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import signup, logout
 
 urlpatterns = [
-    path('signup/', signup, name="signup"),
+    path('accounts/signup/', signup, name="signup"),
     path('accounts/logout/', logout, name="logout")
 ]
